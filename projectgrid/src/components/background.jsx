3b94569186853495a87e27b42d399ba11f00ddf0
Nhlakanipho <nhalakaniphoradebe337@gmail.com>
2024-07-10 05:11:00 -0700

    first commit

---
+++ projectgrid/src/components/background.jsx
@@ -0,0 +1,34 @@
+import "./background.css";
+function Background() {
+  return (
+    <div className="wrapper">
+      <div className="container1">
+        <div className="logo"></div>
+        <div className="Text"></div>
+      </div>
+      <div className="container2">
+        <div className="purple-container">
+          <div className="Navbar">
+            <div className="Product"></div>
+            <div className="Story"></div>
+            <div className="Features"></div>
+            <div className="Contact"></div>
+            <div className="Sign-up"></div>
+          </div>
+          <div className="background-image"></div>
+        </div>
+        <div className="faceTeam">
+          <div className="face"></div>
+          <div className="team"></div>
+          <div className="oliver"></div>
+        </div>
+        <div className="joinSocial">
+        
+          <div className="join"></div>
+          <div className="social"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Background;
