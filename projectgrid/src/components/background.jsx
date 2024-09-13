7342ff78905c8353ed466899eaf3f906d90f5bc5
Nhlakanipho <nhlakaniphoradebe337@gmail.com>
2024-09-13 12:41:44 -1100

    Refractoring:updating the right container layout

--- projectgrid/src/components/background.jsx
+++ projectgrid/src/components/background.jsx
@@ -1,58 +1,46 @@
 import '../components/background.css'
 import { FaHandshake } from "react-icons/fa";
-import { MdArrowOutward } from "react-icons/md";
+// import { MdArrowOutward } from "react-icons/md";
+import Eve from "../assets/Eve.png"
+import Oliver from '../assets/oliver.png'
 
 const background = () => {
   
   return ( 
-    <div className="wrapper">
-      <div className="container">
-        <div className="left-container">
-          <div></div>
-
-        </div>
-      <div className='right-container'>
-        
-        <div className="hero">
-          <div className="navbar">
+     <div className='wrapper'>
+      <div className="container-left"></div>
+      <div className="container-right">
+        <div className='hero'>
+          <div className='navbar'>
             <p>Product</p>
             <p>Story</p>
             <p>Features</p>
-            <p>Contacts</p>
-            <button>Sign-Up</button>
+            <p>Contact</p>
+            <button>Sign Up</button>
+          </div>
+          <div className='Eve-cont'>
+            <img src={Eve} height={"100%"}></img>
           </div>
           <div className='collaborate'>
             <h2>Collaborate</h2>
-             <h2 id="everywhere"><FaHandshake /> everywhere.</h2>
-             <p>Get our free mobile application</p>
-             <button >Download</button>
-
+            <h2><FaHandshake /> everywhere</h2>
+            <p>Get our free mobile application</p>
+            <button>Download</button>
           </div>
         </div>
-          <div className="body">
-
-            <div className="face"></div>
-            <div className="our-team">
-              <div className='team'><h4>Our <span>Team</span></h4></div>
-              <div className='lea'><h3>Lea Herrera</h3></div>
-              <p>UX Designer</p>
-            </div>
-            <div className="oliver">
-              <div className='oliver-text'>
-                <h3>Oliver Haller</h3>
-                <p>Co-Founder <MdArrowOutward /></p>
-              </div>
-            </div>
-
+        <div className='body'>
+          <div className="body-container1"></div>
+          <div className="body-container2"></div>
+          <div className="body-container3">
+          {/* <img src={Oliver} heght={"100%"}></img> */}
           </div>
-          <div className="footer">
-            <div className="footer-1"></div>
-            <div className="footer-2"></div>
-          </div>
-
+        </div>
+        <div className='footer'>
+          <div className='footer-container1'></div>
+          <div className='footer-container2'></div>
         </div>
       </div>
-    </div>
+     </div>
    );
 }
  
