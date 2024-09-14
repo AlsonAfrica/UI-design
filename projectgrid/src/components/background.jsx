7af39ada61f947f86cccd009f48ef255a87b0cdd
Nhlakanipho <nhlakaniphoradebe337@gmail.com>
2024-09-14 03:21:28 -1100

    Feature: left container updated with content

--- projectgrid/src/components/background.jsx
+++ projectgrid/src/components/background.jsx
@@ -3,13 +3,39 @@ import { FaHandshake } from "react-icons/fa";
 import { GoArrowUpRight } from "react-icons/go";
 import Eve from "../assets/Eve.png"
 import { IoIosArrowRoundForward } from "react-icons/io";
+import { FaDotCircle } from "react-icons/fa";
 import Oliver from '../assets/oliver.png'
+import Man from "../assets/man.png"
 
 const background = () => {
   
   return ( 
      <div className='wrapper'>
-      <div className="container-left"></div>
+      <div className="container-left">
+        <div className="logo">
+        <FaDotCircle /> Xpace
+        </div>
+        <div className="bring-text">
+          <p>Team Together</p>
+          <h3>Bring your <br/>team together. <br/> Right here.</h3>
+          <p>The quick brown fox jumps over <br/> the lazy dog</p>
+          <button>Learn how</button>
+        </div>
+
+        <div className="photo-text">
+          <div className="photo">
+            <div className="photo-man">
+            <div className="week5">
+              <p>Week 5</p>
+            </div>
+            <div className='nine-plus'>+9</div>
+            </div>
+          </div>
+          <div className="Xpace"></div>
+        </div>
+      </div>
+      
+      {/* container-right */}
       <div className="container-right">
         <div className='hero'>
           <div className='navbar'>
@@ -24,7 +50,7 @@ const background = () => {
           </div>
           <div className='collaborate'>
             <h2>Collaborate</h2>
-            <h2><FaHandshake /> everywhere</h2>
+            <h2><FaHandshake /> everywhere.</h2>
             <p>Get our free mobile application</p>
             <button>Download</button>
           </div>
@@ -35,7 +61,6 @@ const background = () => {
             <h3 id="our-text">Our <span>Team</span></h3>
             <h3>Lea Herrera</h3>
             <p>UX Designer <GoArrowUpRight /></p>
-           
           </div>
           <div className="body-container3">
             <div className="oliver-text">
@@ -53,14 +78,11 @@ const background = () => {
                   <p>Learn how!</p>
                 </div>
             </div>
-          
           </div>
           <div className='footer-container2'>
             <div className="socials-text">
               <p>Social <br/> Media <br/> Managers <br/> <IoIosArrowRoundForward /></p>
-
             </div>
-
           </div>
         </div>
       </div>
